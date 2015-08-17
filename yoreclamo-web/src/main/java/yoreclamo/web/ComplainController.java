82835f6ab87008224b3a1bd355d70bcdc54699fb
makoto <cmsandiga@gmail.com>
2015-08-17 00:13:19 -0300

    controller for insert complain

--- yoreclamo-web/src/main/java/yoreclamo/web/ComplainController.java
+++ yoreclamo-web/src/main/java/yoreclamo/web/ComplainController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import yoreclamo.domain.Complain;
 import yoreclamo.service.CompanyService;
@@ -36,5 +39,10 @@ public class ComplainController {
 		model.addAttribute("relatedComplains", complainService.findRelatedComplains(id, complain.companyId));
 
 		return "complain_detail";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public @ResponseBody Complain insert(Complain complain) {
+		return complainService.insert(complain);
 	}
 }
