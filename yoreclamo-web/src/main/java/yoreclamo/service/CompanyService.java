7e753615f4d5fd53d362e61b63d76db2d96fd495
makoto <cmsandiga@gmail.com>
2015-08-17 00:10:07 -0300

    Save company service and increment complain company counter

--- yoreclamo-web/src/main/java/yoreclamo/service/CompanyService.java
+++ yoreclamo-web/src/main/java/yoreclamo/service/CompanyService.java
@@ -4,6 +4,10 @@ import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 import yoreclamo.domain.Company;
@@ -14,6 +18,9 @@ public class CompanyService {
 
 	@Autowired
 	private CompanyRepository companyRepository;
+
+	@Autowired
+	private MongoTemplate mongoTemplate;
 
 	public List<Company> findAll() {
 		List<Company> companies = companyRepository.findAll();
@@ -26,5 +33,23 @@ public class CompanyService {
 
 	public Company findOne(String id) {
 		return companyRepository.findOne(id);
+	}
+
+	public Company insert(String name) {
+		Company company = new Company();
+		String normalizedCompany = name.toLowerCase().replace(" ", "-");
+
+		company.setName(name);
+		company.setNameNormalized(normalizedCompany);
+		company.setComplainCounter(0);
+
+		return companyRepository.insert(company);
+	}
+
+	public Company incrementCompanyComplainCounter(String companyId) {
+
+		return mongoTemplate.findAndModify(new Query(Criteria.where("_id").is(companyId)),
+				new Update().inc("complain_counter", 1), Company.class);
+
 	}
 }
